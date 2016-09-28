2c6c8efe40e4bfed3405df90160ed9a882161f4b
Dave Armstrong <davearm1864@gmail.com>
2016-09-28 22:22:15 +0100

    Added 'ref' to form
    Bonded the link

---
+++ client/components/link_create.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+class LinkCreate extends Component {
+  handleSubmit(event){
+    event.preventDefault();
+
+    console.log(this.refs.link.value);
+  }
+
+  render() {
+    return (
+      <div class="row">
+        <form className="col s12" onSubmit={this.handleSubmit.bind(this)}>
+          <div className="row">
+            <div className="input-field col s12">
+              <input ref="link" type="text" id="url" class="validate" />
+              <label for="url">Type or paste your URL</label>
+              <button className="waves-effect waves-light btn-large">Shorten!</button>
+            </div>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default LinkCreate;
